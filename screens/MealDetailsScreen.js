93481706c208107dbe58635db526cedb0e68312c
Dicu Razvan Gabriel <dicu.razvan.gabriel@gmail.com>
2020-03-08 22:33:05 +0200

    added steps and ingredients in meals details

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -3,48 +3,70 @@ import { View, Image, Text, StyleSheet, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import DefaultText from '../components/DefaultText'
+
 const MealDetailsScreen = ({navigation, route}) => {
     const { mealId } = route.params;
-    const selectedMeal =   MEALS.filter( meal => { meal.id === mealId});
+    const selectedMeal =  MEALS.filter( meal => meal.id === mealId)[0];
 
     return(
         <ScrollView>
-            <Image />
-            <View style={{...styles.mealRow, ...styles.mealDetail}}>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+        
+            <View style={styles.detail}>
                 <DefaultText>
-                    {itemData.item.duration}m
+                    {selectedMeal.duration}m
                 </DefaultText>
                 <DefaultText>
-                    {itemData.item.complexity.toUpperCase()}
+                    {selectedMeal.complexity.toUpperCase()}
                 </DefaultText>
                 <DefaultText>
-                    {itemData.item.affordability.toUpperCase()}
+                    {selectedMeal.affordability.toUpperCase()}
                 </DefaultText>
             </View>
-            <View style={styles.screen}>
-                <Text>The MealDetailsScreen  Screen</Text>
+            
+            <Text style={styles.title}>Incredients</Text>
+            {selectedMeal.ingredients.map(ingredient => {
+                return(
+                    <Text key={ingredient}>
+                        {ingredient}
+                    </Text>
+                )
+            })}
+            
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map(step => {
+                return(
+                    <Text key={step}>
+                        {step}
+                    </Text>
+                )
+            })}
+
+            <View style={{height: 100}}>
             </View>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }, 
-
-    mealDetail: {
+    detail: {
+        flexDirection: 'row',
         paddingHorizontal: 10,
         justifyContent: 'space-between',
         alignItems: 'center',
         height: '15%'
     },
 
-    mealRow: {
-        flexDirection: 'row'
+    image: {
+        height: 200,
+        width: '100%'
     },
+
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    }
 })
 
 export default MealDetailsScreen
